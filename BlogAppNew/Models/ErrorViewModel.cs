782a801f6b8efba1050074ec3a35ff784f183eef
ahmettahirmat <210205023@ostimteknik.edu.tr>
2024-04-05 11:33:49 +0300

    Blog App Project

---
+++ BlogAppNew/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogAppNew.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
